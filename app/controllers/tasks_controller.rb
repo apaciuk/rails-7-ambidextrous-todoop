3acb7244c6c8045e8e56c0625619990d86c5cb10
xhostcom <xhostcom@gmail.com>
2022-03-13 14:37:02 +0000

    Gitacora saved 10 changed files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update destroy]
+  before_action :set_task, only: %i[show edit create update destroy]
 
   # GET /tasks or /tasks.json
   def index
